f279e3b8c6f50b15b82d67d29545dc2959b3b78d
disiasd <pietro.dilena@gmail.com>
2025-02-28 12:12:38 +0100

    Add files via upload

--- SortingTest.java
+++ SortingTest.java
@@ -30,73 +30,73 @@ import algorithm.sorting.Sorting;
 
 public class SortingTest {
 
-	private static int[] readarray(String filename) throws FileNotFoundException {
-		ArrayList<Integer> L = new ArrayList<Integer>();
-		Scanner input        = new Scanner(new File(filename));
-		while(input.hasNext()) 
-			L.add(input.nextInt());
-		
-		
-		return L.stream().mapToInt(Integer::intValue).toArray();
-	}
+    private static int[] readarray(String filename) throws FileNotFoundException {
+        ArrayList<Integer> L = new ArrayList<Integer>();
+        Scanner input        = new Scanner(new File(filename));
+        while(input.hasNext()) 
+            L.add(input.nextInt());
+        
+        
+        return L.stream().mapToInt(Integer::intValue).toArray();
+    }
 
-	private static int[] savesorted(int A[]) {
-		int[] B = A.clone();
-		Arrays.sort(B);
-		return B;
-	} 
+    private static int[] savesorted(int A[]) {
+        int[] B = A.clone();
+        Arrays.sort(B);
+        return B;
+    } 
 
-	private static boolean issorted(int A[], int B[]) {
-		for(int i = 0; i < A.length-1; i++)
-			if(A[i] != B[i])
-				return false;
-		return true;
-	}
+    private static boolean issorted(int A[], int B[]) {
+        for(int i = 0; i < A.length-1; i++)
+            if(A[i] != B[i])
+                return false;
+        return true;
+    }
 
-	public static void main(String args[]) {
-		Sorting S   = new Sorting();
+    public static void main(String args[]) {
+        Sorting S   = new Sorting();
         Method  M   = null;
         int     A[] = null;
-		int     B[] = null;
+        int     B[] = null;
 
-		
-		if(args.length != 2) {
-			
-			System.err.println("Usage: SortingTest <filename> <sorting algorithm>\n");
-			System.err.println("Sorting algorithms:");
-			for (Method m : S.getClass().getMethods()) {
-  				if (m.getDeclaringClass() == S.getClass() && m.getParameterTypes()[0].equals(int[].class))
-    				System.err.println("- " + m.getName());
-			}
-			System.exit(0);
-		}
+        
+        if(args.length != 2) {
+            
+            System.err.println("Usage: SortingTest <filename> <sorting algorithm>\n");
+            System.err.println("Sorting algorithms:");
+            for (Method m : S.getClass().getMethods()) {
+                if (m.getDeclaringClass() == S.getClass() && m.getParameterTypes()[0].equals(int[].class))
+                    System.err.println("- " + m.getName());
+            }
+            System.exit(0);
+        }
 
-		try {
-			M = S.getClass().getDeclaredMethod(args[1],int[].class);
-    } catch(Exception e) {
-			System.out.println(e.toString());
-			System.exit(1);
-		}
+        try {
+            M = S.getClass().getDeclaredMethod(args[1],int[].class);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
 
-		try {
-			A = readarray(args[0]);
-			B = savesorted(A);
-		} catch(Exception e) {
-			System.out.println(e.toString());
-			System.exit(1);
-		}
+        try {
+            A = readarray(args[0]);
+            B = savesorted(A);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
 
-		try {
-			long start = System.currentTimeMillis();
-			M.invoke(S,A);
-			long end  = System.currentTimeMillis();
-			System.out.println(Arrays.toString(A));
-			System.out.println("Algorithm: " + args[1] + "    \tTime: " + (end-start)/1000.0 +  "s " + (issorted(A,B) ? "[SORTED]" : "[UNSORTED]"));
-		} catch(Exception e) {
+        try {
+            long start = System.currentTimeMillis();
+            M.invoke(S,A);
+            long end  = System.currentTimeMillis();
+            //System.out.println(Arrays.toString(A)); 
+            System.out.println("Algorithm: " + args[1] + "    \tTime: " + (end-start)/1000.0 +  "s " + (issorted(A,B) ? "[SORTED]" : "[UNSORTED]"));
+        } catch(Exception e) {
             e.printStackTrace();
-			System.exit(1);
-		}
+            System.exit(1);
+        }
 
-	}
-	
+    }
+    
 }
